db64c3b2789a92fbed2c54a7b0d6fd50b16c7352
Marcin Robak <mr.robak@gmail.com>
2025-02-15 17:27:28 +0100

    style shows list

--- src/components/ShowsList.vue
+++ src/components/ShowsList.vue
@@ -11,17 +11,13 @@ const genreShows = computed(() => store.showsByGenre(props.genre))
 
 <template>
   <div>
-    <h2>Shows in "{{ props.genre }}"</h2>
+    <h2>{{ props.genre }}</h2>
     <div v-if="genreShows.length">
-      <div>
-        <ul>
-          <li v-for="show in genreShows" :key="show.id">
-            <img
-              v-if="show.image && show.image"
-              :src="show.image"
-              :alt="show.name"
-            />
-            <div>
+      <div class="cards-container">
+        <ul class="cards-list">
+          <li class="card" v-for="show in genreShows" :key="show.id">
+            <img v-if="show.image" :src="show.image" :alt="show.name" />
+            <div class="card-content">
               <h3>{{ show.name }}</h3>
             </div>
           </li>
@@ -34,4 +30,40 @@ const genreShows = computed(() => store.showsByGenre(props.genre))
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+@import '@/assets/styles/_variables';
+
+.cards-container {
+  overflow-x: auto;
+  padding-bottom: 1rem;
+
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+
+.cards-list {
+  display: flex;
+  gap: 1rem;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.card {
+  border-radius: $card-border-radius;
+  background-color: $card-bg-color;
+  color: #ffffff;
+  flex: 0 0 auto;
+  padding: 1rem;
+}
+
+.card img {
+  max-width: 100%;
+  border-radius: $card-border-radius;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+</style>
