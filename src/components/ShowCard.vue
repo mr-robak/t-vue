dd099b9c23f73210da016dac2cf6325d7435c616
Marcin Robak <mr.robak@gmail.com>
2025-02-16 12:42:36 +0100

    ad image skeleton

--- src/components/ShowCard.vue
+++ src/components/ShowCard.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   name?: string
   summary?: string | null
   image?: string | null
   year?: string | null
   rating?: number | null
 }>()
+
+const noProps = Object.values(props).every((prop) => !prop)
+const hasData = !noProps
 </script>
 
 <template>
-  <div class="card">
-    <img v-if="image" :src="image" :alt="`Poster for the TV show: ${name}`" />
-    <div class="card-content">
-      <h3 v-if="name">{{ name }}</h3>
-      <p v-if="rating">Rating: {{ rating }}</p>
-      <p v-if="year">Year: {{ year }}</p>
-      <p v-if="summary">{{ summary }}</p>
+  <div class="card" :class="{ 'is-loading': noProps }">
+    <div class="card-wrapper">
+      <div v-if="image" class="image-container">
+        <img :src="image" :alt="`Poster for the TV show: ${name}`" />
+      </div>
+
+      <div v-if="hasData" class="card-content">
+        <h3>{{ name }}</h3>
+        <p>Rating: {{ rating }}</p>
+        <p>Year: {{ year }}</p>
+        <p>{{ summary }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -28,25 +36,74 @@ defineProps<{
   background-color: $card-bg-color;
   color: #ffffff;
   flex: 0 0 auto;
-  padding: 1rem;
+
+  &.is-loading {
+    background: linear-gradient(
+      100deg,
+      $card-bg-color 0%,
+      $card-bg-color 40%,
+      lighten($card-bg-color, 5%) 50%,
+      $card-bg-color 60%,
+      $card-bg-color 100%
+    );
+    background-size: 200% 100%;
+    background-position-x: 180%;
+    animation: shine 1.5s linear infinite;
+  }
 }
 
-.card img {
-  display: block;
-  object-fit: cover;
-  width: 100%;
+.card-wrapper {
+  position: relative;
   height: 300px;
+}
+
+.image-container,
+.image-placeholder {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: 100%;
   border-radius: $card-border-radius;
-  margin-bottom: 0.5rem;
+}
+
+.image-container img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: inherit;
+}
+
+.image-placeholder {
+  background-color: lighten($card-bg-color, 5%);
 }
 
 .card-content {
+  position: absolute;
+  inset: 0;
+  padding: 1rem;
+  background: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0.9) 0%,
+    rgba(0, 0, 0, 0.5) 50%,
+    rgba(0, 0, 0, 0) 100%
+  );
+  border-radius: $card-border-radius;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+
   h3 {
     margin: 0.5rem 0;
   }
 
   p {
-    margin: 0;
+    margin: 0.25rem 0;
+  }
+}
+
+@keyframes shine {
+  to {
+    background-position-x: -200%;
   }
 }
 </style>
